af60a4d799111967055444067627eb16feae1f04
Ale Sánchez <alesanchez@us.es>
2018-12-28 00:24:33 +0100

    Removed unused code
    Removed unused logging lines and variables

--- skill/handlers/LoggingRequestInterceptor.py
+++ skill/handlers/LoggingRequestInterceptor.py
@@ -5,10 +5,9 @@ from utils import logger
 
 class LoggingRequestInterceptor(AbstractRequestInterceptor):
     def process(self, handler_input):
-        attr = handler_input.attributes_manager.session_attributes
         intent_received = 'Unknown'
 
-        logger.get_logger().info('Request: {}'.format(handler_input.request_envelope))
+        # logger.get_logger().info('Request: {}'.format(handler_input.request_envelope))
 
         if handler_input.request_envelope.request.object_type == 'IntentRequest':
             intent_received = handler_input.request_envelope.request.intent.name
@@ -16,4 +15,3 @@ class LoggingRequestInterceptor(AbstractRequestInterceptor):
             intent_received = handler_input.request_envelope.request.object_type
 
         logger.get_logger().info('Intent received: {}'.format(intent_received))
-        logger.get_logger().info('User state: {}'.format(attr.get('state', None)))
